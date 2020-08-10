4616a4123e7db36c47e861ff15e96922040ac2f6
JinhaJjing <horseesroh@naver.com>
2020-08-10 16:16:55 +0900

    DFS

---
+++ Algorithm/Solved/타겟 넘버.cpp
@@ -0,0 +1,24 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/43165
+// dfs
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+
+void dfs(vector<int> numbers, int target, int count, int sum) {
+	if (count == numbers.size()) {
+		if (sum == target)	answer++;
+		return;
+	}
+	dfs(numbers, target, count+1, sum - numbers[count]);
+	dfs(numbers, target, count+1, sum + numbers[count]);
+	//count++로 했다가 계속 터져서 원인 찾느라 진짜...후(심한말)
+}
+
+int solution(vector<int> numbers, int target) {
+	dfs(numbers, target, 0, 0);
+	return answer;
+}
