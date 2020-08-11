f53261556275c85ad1a85eecc5200621ccccbe36
JinhaJjing <horseesroh@naver.com>
2020-08-11 14:41:34 +0900

    DFS

--- Algorithm/Unsolved/단어 변환.cpp
+++ Algorithm/Unsolved/단어 변환.cpp
@@ -3,19 +3,22 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int answer = 0;
+//테케중에 시간초과나는 것 때문에 실패했는데 이유 모름
+
+int answer = 50;
 
 void dfs(int cnt, vector<string> words, vector<bool> visited, string begin, string target) {
 	for (int i = 0; i < words.size(); i++) {
 		if (!visited[i]) {
 			int sameDigits = 0;
 			for (int j = 0; j < begin.length(); j++)
-				if (begin[j] != words[i][j])
+				if (begin[j] == words[i][j])
 					sameDigits++;
-			if (sameDigits == 1) {
+			if (sameDigits >= 2) {
 				//target완성
 				if (target == words[i] && answer > cnt + 1) {
 					answer = cnt + 1;
@@ -35,7 +38,12 @@ void dfs(int cnt, vector<string> words, vector<bool> visited, string begin, stri
 
 int solution(string begin, string target, vector<string> words) {
 	vector<bool> visited(50);
+	sort(words.begin(), words.end());
 	dfs(0, words, visited, begin, target);
-
+	if (answer == 50) return 0;
 	return answer;
+}
+
+int main() {
+	solution("hit", "cog", { "hot", "dot", "dog", "lot", "log", "cog" });
 }
