6c6774011ba3647ee4ca1b573a4a9e59b64de7c4
JinhaJjing <horseesroh@naver.com>
2020-08-12 17:49:10 +0900

    Stack

---
+++ Algorithm/Solved/짝지어 제거하기.cpp
@@ -0,0 +1,26 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/12973
+// stack
+
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+	int answer = 0;
+	stack<char> numStack;
+
+	for (int i = 0; i < s.length(); i++) {
+		if (numStack.size() >= 1 && s[i] == numStack.top())
+			numStack.pop();
+		else numStack.push(s[i]);
+		if (numStack.empty() && i == s.length() - 1) return 1;
+	}
+	return 0;
+}
+
+int main() {
+	solution("baabaa");
+	solution("cdccdc");
+}
