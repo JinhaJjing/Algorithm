ba45d6e480ea9fb9ce455b318128d5ab57bb26e6
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2020-08-24 20:18:37 +0900

    update

---
+++ Algorithm/Later/스티커 모으기(2).cpp
@@ -0,0 +1,21 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12971
+// 
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//접근하기 어렵다 했더니 역시
+//dp. dp식 세우는건 연습이 안되네
+
+int solution(vector<int> sticker)
+{
+	int answer = 36;
+
+
+
+
+
+	return answer;
+}
