ffd6a652ea08d97805f88ec118ece300fff5572a
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2020-09-10 15:46:10 +0900

    stack

--- Algorithm/Solved/쇠막대기.cpp
+++ Algorithm/Solved/쇠막대기.cpp
@@ -1,32 +1,39 @@
 ﻿// https://programmers.co.kr/learn/courses/30/lessons/42585
 // 스택
 
+#include <iostream>
 #include <string>
-#include <vector>
 #include <stack>
-
-//TODO : 스택 hard
 
 using namespace std;
 
-int solution(string arrangement) {
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string arrangement;
+	cin >> arrangement;
+
 	int answer = 0;
 	stack<char> parenthesis;
-	vector<char> state;
 
 	for (int i = 0; i < arrangement.size(); i++) {
 		if (arrangement[i] == '(')
 			parenthesis.push(arrangement[i]);
 		else {
-			if (parenthesis.top() == '(') {//레이저랑 파이프 구별하기
+			if (arrangement[i-1]=='(') {//레이저랑 파이프 구별하기
 				parenthesis.pop();
-				state[i] = '*';//레이저
+				answer += (int)parenthesis.size();
+			}
+			else {
+				parenthesis.pop();
+				answer += 1;
 			}
 		}
 	}
 
+	cout << answer;
 
-
-
-	return answer;
+	return 0;
 }
